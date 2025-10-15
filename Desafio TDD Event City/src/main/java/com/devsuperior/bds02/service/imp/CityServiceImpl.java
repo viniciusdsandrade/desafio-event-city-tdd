1b68cb1ad3142c628af2c2d1962ad889527722f4
andrade <viniciusdsandrade0662@gmail.com>
2025-10-15 12:13:14 -0300

    refactor: rename repository variable and simplify sorting in CityServiceImpl

--- Desafio TDD Event City/src/main/java/com/devsuperior/bds02/service/imp/CityServiceImpl.java
+++ Desafio TDD Event City/src/main/java/com/devsuperior/bds02/service/imp/CityServiceImpl.java
@@ -7,27 +7,28 @@ import com.devsuperior.bds02.exception.ResourceNotFoundException;
 import com.devsuperior.bds02.repository.CityRepository;
 import com.devsuperior.bds02.service.CityService;
 import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 import static java.util.stream.Collectors.toList;
+import static org.springframework.data.domain.Sort.Order.asc;
+import static org.springframework.data.domain.Sort.by;
 
 @Service
 public class CityServiceImpl implements CityService {
 
-    private final CityRepository repository;
+    private final CityRepository cityRepository;
 
-    public CityServiceImpl(CityRepository repository) {
-        this.repository = repository;
+    public CityServiceImpl(CityRepository cityRepository) {
+        this.cityRepository = cityRepository;
     }
 
     @Transactional(readOnly = true)
     @Override
     public List<CityDTO> findAllSortedByName() {
-        List<City> list = repository.findAll(Sort.by("name"));
+        List<City> list = cityRepository.findAll(by(asc("name").ignoreCase()));
         return list.stream()
                 .map(CityDTO::new)
                 .collect(toList());
@@ -37,19 +38,18 @@ public class CityServiceImpl implements CityService {
     @Override
     public CityDTO insert(CityDTO dto) {
         City entity = new City(dto.name());
-        entity = repository.save(entity);
+        cityRepository.save(entity);
         return new CityDTO(entity);
     }
 
     @Transactional
     @Override
     public void delete(Long id) {
-        if (!repository.existsById(id)) {
-            throw new ResourceNotFoundException("City id not found " + id);
-        }
+        if (!cityRepository.existsById(id)) throw new ResourceNotFoundException("City id not found " + id);
+
         try {
-            repository.deleteById(id);
-            repository.flush();
+            cityRepository.deleteById(id);
+            cityRepository.flush();
         } catch (DataIntegrityViolationException e) {
             throw new DatabaseException("Integrity violation");
         }
